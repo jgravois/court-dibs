c8cab3126a0581628046242ba19116e65e8763ba
john gravois <jagravois@gmail.com>
2024-03-19 22:30:27 -0700

    wire up address search with autocomplete

--- app/routes/join.tsx
+++ app/routes/join.tsx
@@ -1,3 +1,4 @@
+import { Loader } from "@googlemaps/js-api-loader";
 import type {
   ActionFunctionArgs,
   LoaderFunctionArgs,
@@ -11,35 +12,53 @@ import { createUser, getUserByEmail } from "~/models/user.server";
 import { createUserSession, getUserId } from "~/session.server";
 import { safeRedirect, validateCoordinates, validateEmail } from "~/utils";
 
+const HALF = "AIzaSyBI_vhCo";
+
 export const loader = async ({ request }: LoaderFunctionArgs) => {
   const userId = await getUserId(request);
   if (userId) return redirect("/");
   return json({});
 };
 
+const OTHER_HALF = "hiRS0dvt5Yk7sAJ-978T_mUwd8";
+
 export const action = async ({ request }: ActionFunctionArgs) => {
   const formData = await request.formData();
   const email = formData.get("email");
   const password = formData.get("password");
+  const rawCoordinates = formData.get("coordinates");
+
   const redirectTo = safeRedirect(formData.get("redirectTo"), "/");
 
   if (!validateEmail(email)) {
     return json(
-      { errors: { email: "Email is invalid", password: null } },
+      { errors: { email: "Email is invalid", password: null, address: null } },
       { status: 400 },
     );
   }
 
   if (typeof password !== "string" || password.length === 0) {
     return json(
-      { errors: { email: null, password: "Password is required" } },
+      {
+        errors: {
+          email: null,
+          password: "Password is required",
+          address: null,
+        },
+      },
       { status: 400 },
     );
   }
 
   if (password.length < 8) {
     return json(
-      { errors: { email: null, password: "Password is too short" } },
+      {
+        errors: {
+          email: null,
+          password: "Password is too short",
+          address: null,
+        },
+      },
       { status: 400 },
     );
   }
@@ -51,20 +70,34 @@ export const action = async ({ request }: ActionFunctionArgs) => {
         errors: {
           email: "A user already exists with this email",
           password: null,
+          address: null,
         },
       },
       { status: 400 },
     );
   }
 
-  const coordinates: [number, number] = [-115.68, 33.4825];
+  if (typeof rawCoordinates !== "string" || rawCoordinates.length === 0) {
+    return json(
+      {
+        errors: {
+          email: null,
+          password: null,
+          address: "Street address is required",
+        },
+      },
+      { status: 400 },
+    );
+  }
 
+  const coordinates = rawCoordinates?.split(",") as unknown as [number, number];
   if (!validateCoordinates(coordinates)) {
     return json(
       {
         errors: {
-          email: "signup is only available to HOA residents",
+          email: null,
           password: null,
+          address: "signup is only available to HOA residents",
         },
       },
       { status: 400 },
@@ -89,6 +122,10 @@ export default function Join() {
   const actionData = useActionData<typeof action>();
   const emailRef = useRef<HTMLInputElement>(null);
   const passwordRef = useRef<HTMLInputElement>(null);
+  const addressRef = useRef<HTMLInputElement>(null);
+  const coordinatesRef = useRef<HTMLInputElement>(null);
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const autoCompleteRef = useRef<any>(null);
 
   useEffect(() => {
     if (actionData?.errors?.email) {
@@ -97,6 +134,31 @@ export default function Join() {
       passwordRef.current?.focus();
     }
   }, [actionData]);
+
+  const options = { fields: ["geometry"] };
+
+  useEffect(() => {
+    const loader = new Loader({
+      apiKey: HALF + OTHER_HALF,
+      version: "weekly",
+    });
+
+    loader.load().then(async (goo) => {
+      const { Autocomplete } = await goo.maps.importLibrary("places");
+      autoCompleteRef.current = new Autocomplete(addressRef.current, options);
+
+      autoCompleteRef.current?.addListener("place_changed", async function () {
+        if (autoCompleteRef.current && coordinatesRef.current) {
+          const { geometry } = await autoCompleteRef.current.getPlace();
+          coordinatesRef.current.value = `${geometry.location.lng()},${geometry.location.lat()}`;
+        }
+      });
+    });
+
+    return () => {
+      autoCompleteRef.current = null;
+    };
+  });
 
   return (
     <div className="flex min-h-full flex-col justify-center">
@@ -157,6 +219,37 @@ export default function Join() {
             </div>
           </div>
 
+          <div>
+            <label
+              htmlFor="street-address"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Street Address
+            </label>
+            <div className="mt-1">
+              <input
+                id="street-address"
+                ref={addressRef}
+                name="street-address"
+                type="text"
+                autoComplete="off"
+                aria-invalid={actionData?.errors?.address ? true : undefined}
+                aria-describedby="street-address-error"
+                className="w-full rounded border border-gray-500 px-2 py-1 text-lg"
+              />
+              {actionData?.errors?.address ? (
+                <div className="pt-1 text-red-700" id="password-error">
+                  {actionData.errors.address}
+                </div>
+              ) : null}
+            </div>
+          </div>
+          <input
+            type="text"
+            name="coordinates"
+            ref={coordinatesRef}
+            style={{ display: "none" }}
+          />
           <input type="hidden" name="redirectTo" value={redirectTo} />
           <button
             type="submit"
