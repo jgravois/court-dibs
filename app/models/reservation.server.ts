b84d024aa8197cd9b305d9bec421eb7250330ff1
john gravois <jagravois@gmail.com>
2024-06-15 10:21:55 -0700

    no validation

--- app/models/reservation.server.ts
+++ app/models/reservation.server.ts
@@ -33,13 +33,13 @@ export async function createReservation({
 }: Pick<Reservation, "start" | "end" | "court" | "openPlay"> & {
   userId: User["id"];
 }) {
-  if (differenceInMinutes(end, start) > 120) {
-    throw new Error('Reservations must be two hours or less')
-  }
+  // if (differenceInMinutes(end, start) > 120) {
+  //   throw new Error('Reservations must be two hours or less')
+  // }
 
-  if (compareAsc(addDays(startOfToday(), 7), start) === -1) {
-    throw new Error('Reservations more than seven days in the future are not allowed')
-  }
+  // if (compareAsc(addDays(startOfToday(), 7), start) === -1) {
+  //   throw new Error('Reservations more than seven days in the future are not allowed')
+  // }
 
   // if (compareAsc(start, new Date()) === -1) {
   //   throw new Error('You\'re livin in the past dude')
@@ -53,44 +53,44 @@ export async function createReservation({
   //   throw new Error('Reservations cannot be made until the top of the hour')
   // }
 
-  if (compareAsc(addDays(startOfToday(), 7), start) === -1) {
-    throw new Error('Reservations more than seven days away are not allowed')
-  }
+  // if (compareAsc(addDays(startOfToday(), 7), start) === -1) {
+  //   throw new Error('Reservations more than seven days away are not allowed')
+  // }
 
-  if (start.getHours() < 8) {
-    throw new Error('Reservations before 8:00 are not allowed')
-  }
+  // if (start.getHours() < 8) {
+  //   throw new Error('Reservations before 8:00 are not allowed')
+  // }
 
-  // TODO: warn if after dusk
-  if (compareAsc(end, addHours(startOfDay(start), 20)) === 1) {
-    throw new Error('Reservations must conclude by 20:00')
-  }
+  // // TODO: warn if after dusk
+  // if (compareAsc(end, addHours(startOfDay(start), 20)) === 1) {
+  //   throw new Error('Reservations must conclude by 20:00')
+  // }
 
-  const overlaps = await prisma.reservation.findFirst({
-    where: {
-      AND: { court },
-      OR: {
-        end: { gt: start },
-        start: { lt: end }
-      }
-    }
-  })
+  // const overlaps = await prisma.reservation.findFirst({
+  //   where: {
+  //     AND: { court },
+  //     OR: {
+  //       end: { gt: start },
+  //       start: { lt: end }
+  //     }
+  //   }
+  // })
 
-  if (overlaps) throw new Error('This would overlap an existing reservation')
+  // if (overlaps) throw new Error('This would overlap an existing reservation')
 
-  const sameDay = await prisma.reservation.findFirst({
-    where: {
-      userId,
-      court,
-      start: {
-        gte: startOfDay(start),
-        lte: addDays(startOfDay(start), 1)
-      }
+  // const sameDay = await prisma.reservation.findFirst({
+  //   where: {
+  //     userId,
+  //     court,
+  //     start: {
+  //       gte: startOfDay(start),
+  //       lte: addDays(startOfDay(start), 1)
+  //     }
 
-    }
-  })
+  //   }
+  // })
 
-  if (sameDay) throw new Error('Each court can only be reserved once per day')
+  // if (sameDay) throw new Error('Each court can only be reserved once per day')
 
   return prisma.reservation.create({
     data: {
