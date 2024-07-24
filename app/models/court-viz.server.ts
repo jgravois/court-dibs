65d0b6d91d415b9cfcc129202e468ae7278a13e5
john gravois <jagravois@gmail.com>
2024-07-24 11:43:38 -0700

    use upsert

--- app/models/court-viz.server.ts
+++ app/models/court-viz.server.ts
@@ -4,15 +4,10 @@ import { prisma } from "~/db.server";
 
 export type { CourtViz } from "@prisma/client";
 
-export async function createCourtViz({ userId, hidePb, hide10s, hideBball }: { userId: User["id"]; hidePb: boolean; hide10s: boolean; hideBball: boolean }) {
-  return prisma.courtViz.create({ data: { userId, hidePb, hide10s, hideBball } });
-}
-
-export async function updateCourtViz({ userId, hidePb, hide10s, hideBball }: { userId: User["id"]; hidePb: boolean; hide10s: boolean; hideBball: boolean }) {
-  return prisma.courtViz.update({
-    where: { userId },
-    data: { hidePb, hide10s, hideBball },
+export async function upsertCourtViz({ userId, hidePb, hide10s, hideBball }: { userId: User["id"]; hidePb: boolean; hide10s: boolean; hideBball: boolean }) {
+  const update = { hidePb, hide10s, hideBball }
+  return prisma.courtViz.upsert({
+    where: { userId }, update, create: { userId, ...update },
   });
 }
-
 
--- app/routes/some.route.tsx
+++ app/routes/some.route.tsx
@@ -1,8 +1,7 @@
 import type { ActionFunctionArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
 
-import { createCourtViz, updateCourtViz } from "~/models/court-viz.server";
-import { getUserById } from "~/models/user.server";
+import { upsertCourtViz } from "~/models/court-viz.server";
 import { requireUserId } from "~/session.server";
 
 export const action = async ({ request }: ActionFunctionArgs) => {
@@ -10,20 +9,12 @@ export const action = async ({ request }: ActionFunctionArgs) => {
   const formData = await request.formData();
 
   try {
-    const opts = {
+    await upsertCourtViz({
       userId,
       hidePb: formData.get("hidePb") !== "on",
       hide10s: formData.get("hide10s") !== "on",
       hideBball: formData.get("hideBball") !== "on",
-    };
-
-    const user = await getUserById(userId);
-
-    if (user?.courtViz) {
-      updateCourtViz(opts);
-    } else {
-      await createCourtViz(opts);
-    }
+    });
   } catch (e) {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     return json({ errors: { start: (e as any).message } }, { status: 400 });
